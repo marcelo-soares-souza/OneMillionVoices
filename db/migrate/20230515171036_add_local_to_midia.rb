aeefa6620b333ad64f180ae3824df2047cbfd9ca
Marcelo Soares Souza <marcelo@agroecologymap.org>
2023-05-15 16:27:51 -0300

    removed required in media entity

--- db/migrate/20230515171036_add_local_to_midia.rb
+++ db/migrate/20230515171036_add_local_to_midia.rb
@@ -2,6 +2,6 @@
 
 class AddLocalToMidia < ActiveRecord::Migration[7.0]
   def change
-    add_reference :midias, :local, null: false, foreign_key: true
+    add_reference :midias, :local, foreign_key: true
   end
 end
