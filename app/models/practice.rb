0c18a4259f75e4812993239d15f39e47f3c555bf
Marcelo Soares Souza <marcelo@agroecologymap.org>
2023-05-18 08:46:35 -0300

    Flex Layout for Nav Tabs for better Mobile Experience

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -14,7 +14,7 @@ class Practice < ApplicationRecord
   belongs_to :usuario
   belongs_to :local
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 
   extend FriendlyId
   friendly_id :name, use: :slugged
