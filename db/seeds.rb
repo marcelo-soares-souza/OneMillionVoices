49e771a18384cbe9e3a8546b7d402c19fb15df7a
Marcelo Soares Souza <marcelo@agroecologymap.org>
2023-05-29 13:19:40 -0300

    More Improvements in Layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-account_admin = Account.new(name: "Admin Agroecology Map", email: "admin@agroecologymap.org", password: "agroecology", admin: "t")
-account_admin.password_confirmation = "agroecology"
+password = ENV.fetch("ADMIN_PASSWORD") { "agroecology" }
+
+account_admin = Account.new(name: "One Million Voices Administrator", email: "admin@agroecologymap.org", password: password, admin: "t")
+account_admin.password_confirmation = password
 account_admin.skip_confirmation!
 account_admin.save!
 
-account_agroecologia = Account.new(name: "Agroecology Map", email: "contact@agroecologymap.org", password: "agroecology", admin: "f")
-account_agroecologia.password_confirmation = "agroecology"
+account_agroecologia = Account.new(name: "One Million Voices", email: "contact@agroecologymap.org", password: password, admin: "f")
+account_agroecologia.password_confirmation = password
 account_agroecologia.skip_confirmation!
 account_agroecologia.save!
