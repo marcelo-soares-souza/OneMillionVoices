42b0f09f7faaba7419ddb1d9586c0908397b126f
Marcelo Soares Souza <marcelo@agroecologymap.org>
2023-06-02 17:03:37 -0300

    Added GIN Index for Name using pg_trgm

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_01_130831) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_194901) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
@@ -41,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_130831) do
     t.boolean "i_agree_with_terms_and_conditions"
     t.index ["confirmation_token"], name: "index_accounts_on_confirmation_token", unique: true
     t.index ["email"], name: "index_accounts_on_email", unique: true
+    t.index ["name"], name: "index_accounts_on_name", opclass: :gin_trgm_ops, using: :gin
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_accounts_on_slug", unique: true
   end
@@ -152,6 +154,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_130831) do
     t.boolean "is_it_a_farm"
     t.string "continent"
     t.index ["account_id"], name: "index_locations_on_account_id"
+    t.index ["name"], name: "index_locations_on_name", opclass: :gin_trgm_ops, using: :gin
     t.index ["slug"], name: "index_locations_on_slug", unique: true
   end
 
@@ -176,6 +179,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_130831) do
     t.string "slug"
     t.index ["account_id"], name: "index_practices_on_account_id"
     t.index ["location_id"], name: "index_practices_on_location_id"
+    t.index ["name"], name: "index_practices_on_name", opclass: :gin_trgm_ops, using: :gin
     t.index ["slug"], name: "index_practices_on_slug", unique: true
   end
 
