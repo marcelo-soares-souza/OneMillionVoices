44a198356318e0958182440028dccd397b0d8e8a
Marcelo Soares Souza <marcelo@agroecologymap.org>
2023-06-04 16:54:56 -0300

    Improvement in URL for Locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,10 @@ class ApplicationController < ActionController::Base
       end
     end
 
+    def default_url_options(options = {})
+      { locale: I18n.locale }
+    end
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,63 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "errors/not_found"
-  get "errors/internal_server_error"
+  scope "(:locale)", locale: /en|pt-BR|es|fr/ do
+    get "errors/not_found"
+    get "errors/internal_server_error"
 
-  scope "(:locale)", locale: /pt-BR|es|en|fr|gl/ do
     root to: "home#index"
     get "home/index"
-  end
 
-  get "map", to: "home#map"
-  get "landing", to: "about#landing"
-  get "about", to: "about#index"
-  get "who_we_are", to: "about#who_we_are"
-  get "manual", to: "about#manual"
-  get "license", to: "about#license"
-  get "thank_you_notes", to: "about#thank_you_notes"
-  get "connect", to: "connect#index"
+    get "map", to: "home#map"
+    get "landing", to: "about#landing"
+    get "about", to: "about#index"
+    get "who_we_are", to: "about#who_we_are"
+    get "manual", to: "about#manual"
+    get "license", to: "about#license"
+    get "thank_you_notes", to: "about#thank_you_notes"
+    get "connect", to: "connect#index"
 
 
-  get "coordinates", to: "locations#coordinates"
-  get "countries", to: "locations#countries"
-  put "/practices/:id/like", to: "practices#like", as: "like"
+    get "coordinates", to: "locations#coordinates"
+    get "countries", to: "locations#countries"
+    put "/practices/:id/like", to: "practices#like", as: "like"
 
-  resources :practices
-  resources :acknowledges
-  resources :evaluates
-  resources :characterises
-  resources :what_you_dos
-  resources :comments
-
-  resources :practices do
+    resources :practices
     resources :acknowledges
     resources :evaluates
     resources :characterises
     resources :what_you_dos
-    resources :medias
-    resources :documents
     resources :comments
-    get "/gallery" => "medias#gallery"
+
+    resources :practices do
+      resources :acknowledges
+      resources :evaluates
+      resources :characterises
+      resources :what_you_dos
+      resources :medias
+      resources :documents
+      resources :comments
+      get "/gallery" => "medias#gallery"
+    end
+
+    resources :locations do
+      get "/gallery" => "medias#gallery"
+      resources :practices
+      resources :medias
+      resources :documents
+    end
+
+    devise_for :accounts, controllers: {
+      registrations: "accounts/registrations", sessions: "accounts/sessions"
+    }
+
+    resources :accounts do
+      resources :locations
+      resources :practices
+    end
+
+    match "/404", to: "errors#not_found", via: :all
+    match "/500", to: "errors#internal_server_error", via: :all
+    match "/422", to: "errors#unprocessable_entity", via: :all
   end
-
-  resources :locations do
-    get "/gallery" => "medias#gallery"
-    resources :practices
-    resources :medias
-    resources :documents
-  end
-
-  devise_for :accounts, controllers: {
-    registrations: "accounts/registrations", sessions: "accounts/sessions"
-  }
-
-  resources :accounts do
-    resources :locations
-    resources :practices
-  end
-
-  match "/404", to: "errors#not_found", via: :all
-  match "/500", to: "errors#internal_server_error", via: :all
-  match "/422", to: "errors#unprocessable_entity", via: :all
 end
